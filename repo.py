2765e9077b9d787b09cf52eef7d33bedff3358d7
ahmednabiled <hhtg250@gmail.com>
2023-08-30 18:20:22 +0300

    the latest commint :)

--- repo.py
+++ repo.py
@@ -1,3 +1,6 @@
 # this is just a normal code that generates numbers 
 for i in range(1,11):
     print(f"[{str(i).zfill(2)}]")
+
+# really happy that my code is working just fine 
+# and it is working with github
